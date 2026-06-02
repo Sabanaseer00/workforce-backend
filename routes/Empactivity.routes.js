8136f8179926172387b93f82bc6b8804ea0e7bb3
beenishlatif <beenishlatif1026@gmail.com>
2026-06-02 09:12:37 +0500

    fix: correct workhours export name

--- routes/Empactivity.routes.js
+++ routes/Empactivity.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware.js";
 import { getMyActivity } from "../controllers/Empactivity.controller.js";
-import { getWorkHours } from "../controllers/Empworkhours.controller.js";
+import { getMyWorkHours } from "../controllers/Empworkhours.controller.js";
 
 const router = express.Router();
 
 router.get("/activity", protect, getMyActivity);
-router.get("/workhours", protect, getWorkHours);
+router.get("/workhours", protect, getMyWorkHours);
 
 export default router;
